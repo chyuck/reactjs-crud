f11af4115f8f5565625c73f789d432adcbb47aea
Andrey Sergeev <andrey@chyuck.com>
2019-09-04 19:54:54 -0400

    Cleaned up and added router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import ListOrders from "./components/ListOrders";
+import CreateOrder from "./components/CreateOrder";
+import EditOrder from "./components/EditOrder";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={ListOrders} />
+          <Route path="/create" component={CreateOrder} />
+          <Route path="/edit/:id" component={EditOrder} />
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
